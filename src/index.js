35c4529d8be07a337ec51af32b066cae34ab71e9
mafergus <matthew.n.ferguson@gmail.com>
2018-10-14 22:29:49 +0300

    Reworking exports

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
 import PhoneNumberTextField from 'components/PhoneNumberTextField';
 import PhoneNumberInput from 'components/PhoneNumberInput';
-import Form from 'components/ui/form/Form';
-import Text from 'components/ui/form/Text';
-import ControlledTextInput from './components/ui/form/ControlledTextInput';
-import TextInput from 'components/ui/form/TextInput';
-import Phone from 'components/ui/form/Phone';
-import ConfirmPassword from './components/ui/form/ConfirmPassword';
-import Password from 'components/ui/form/Password';
 import Button from 'components/ui/Button';
 import AppBar from 'components/ui/AppBar';
-import Footer from 'components/ui/footer/Footer';
-import Link from 'components/ui/footer/Link';
-import PostFooter from 'components/ui/footer/PostFooter';
-import Section from 'components/ui/footer/Section';
-import Title from 'components/ui/footer/Title';
 
 export {
   PhoneNumberTextField,
   PhoneNumberInput,
-  Form,
-  ControlledTextInput,
-  Text,
-  TextInput,
-  Phone,
-  ConfirmPassword,
-  Password,
   Button,
   AppBar,
-  Footer,
-  Link,
-  PostFooter,
-  Section,
-  Title,
 };
